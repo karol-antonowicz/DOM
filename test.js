310d0e9530ffd413a142de9566f4faec15bbbeb1
Karol Antonowicz <karol.antonowicz@gmail.com>
2019-10-05 23:55:51 +0200

    initial commit: added test.js with some DOM elements usage

---
+++ test.js
@@ -0,0 +1,14 @@
+document.getElementsByClassName("content");
+
+var myContentDivs =  document.getElementsByClassName("content");
+
+myContentDivs
+
+var myH2 = myContentDivs[1].getElementsByTagName("h2");
+
+myH2
+
+myH2[0].innerHTML = "YO MAN WUDDDUP??"
+
+document.getElementById("page-title");
+
